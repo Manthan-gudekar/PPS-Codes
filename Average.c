734c20fbf2053727aa37836d5637a407e91be830
Manthan-gudekar <gudekarmanthan30@gmail.com>
2025-05-05 10:44:41 +0530

    Create Average.c

---
+++ Average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,avg;
+    
+    printf("Enter First Number\t");
+    scanf("%f",&num1);
+    
+    printf("Enter Second Number\t");
+    scanf("%f",&num2);
+    
+    avg=(num1+num2)/2;
+    printf("The avg of %f and %f is %f",num1,num2,avg);
+    
+    return 0;
+}
