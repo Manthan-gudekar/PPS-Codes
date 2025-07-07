0e5bdc8e4312804e371c64be4332460a3f4887b9
Manthan-gudekar <gudekarmanthan30@gmail.com>
2025-07-07 19:57:50 +0530

    Update odd even.c

--- odd even.c
+++ odd even.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int num,rem;
-printf("Enter the number:\t);
-scanf("%d",&num);
-rem=num%2
-if(rem==0)
-{
-printf("The entered number %d is EVEN",num);
-}
-else{
-printf("The entered number %d is ODD",num);
-}
-return 0.;
+    int num,rem;
+	printf("Enter the Number : ");
+	scanf("%d", &num);
+	rem=num%2;
+	if (rem==0)
+	{
+		printf("The entered number %d is even",num);
+	}
+	else
+	{
+		printf("The entered number %d is odd",num);
+	}	
+	return 0;
 }
